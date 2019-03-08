2899aa3c972bf4dc90f4952a29f809089728f75a
Hektor Misplon <hektor.misplon@gmail.com>
2019-03-08 19:59:38 +0100

    show user info

--- tinder/index.py
+++ tinder/index.py
@@ -1,27 +1,27 @@
-# import json
-# with open('data.json', 'r') as data:
-#     json_data = json.load(data)
-
-# # for user in json_data:
-#     # print(json_data['results'])
-#     # print(json_data['results'])
-
-# print(json_data['results'][0]['gender'])
-
 import urllib.request
 import json
+
+from sense_hat import SenseHat
+s = SenseHat()
 
 
 def show_info(*infos):
     for info in infos:
-        print(info)
+        print('displaying: ' + info)
+        s.show_message(info)
 
 
-with urllib.request.urlopen('https://randomuser.me/api/?=1') as rand_user_api:
-    users = json.loads(rand_user_api.read().decode())
-    user = users['results'][0]
+def get_user():
+    with urllib.request.urlopen('https://randomuser.me/api/?=1') as rand_user_api:
+        users = json.loads(rand_user_api.read().decode())
+        return users['results'][0]
+
+
+def get_info(user):
+    name = user['name']['first'] + ' ' + user['name']['last']
     gender = user['gender']
-    name = user['name']['first'] + ' ' + \
-        users['results'][0]['name']['last']
     city = user['location']['city']
     show_info(name, gender, city)
+
+
+get_info(get_user())

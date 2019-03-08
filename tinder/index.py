557d7b5960f5b0d34a7b6ac47a7d2f8c4a8fc015
Hektor Misplon <hektor.misplon@gmail.com>
2019-03-08 20:02:40 +0100

    show user info

--- tinder/index.py
+++ tinder/index.py
@@ -4,24 +4,20 @@ import json
 from sense_hat import SenseHat
 s = SenseHat()
 
-
 def show_info(*infos):
     for info in infos:
         print('displaying: ' + info)
         s.show_message(info)
-
 
 def get_user():
     with urllib.request.urlopen('https://randomuser.me/api/?=1') as rand_user_api:
         users = json.loads(rand_user_api.read().decode())
         return users['results'][0]
 
-
 def get_info(user):
     name = user['name']['first'] + ' ' + user['name']['last']
     gender = user['gender']
     city = user['location']['city']
     show_info(name, gender, city)
-
 
 get_info(get_user())
